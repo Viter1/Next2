0ddc599fc61a07e280b23b617fc18cd04c4ef2f2
DiViteri <DiViteri@10.34.86.88>
2018-12-03 17:05:33 +0100

    dos

--- src/Arrays_Y_Strings/EjClase1.java
+++ src/Arrays_Y_Strings/EjClase1.java
@@ -6,13 +6,19 @@ public class EjClase1 {
 	public static String inReverse(int[] values) {
 		String message = "";
 		int aux[] = new int[values.length];
-
-		for (int i = 0; i < values.length; i++) {
-			aux[i] = values[values.length - 1 - i];
-
+//		for (int i = values.length ; i >=0; i--) {
+//			message += values[i] + " ";
+//		}
+//		for (int i = 0; i < values.length; i++) {
+//			aux[i] = values[values.length - 1 - i];
+//			message += aux[i] + " ";
+//
+//		}
+		for (int i : values) {
+			message = i + " " + message;
 		}
-		String valores = Arrays.toString(aux);
-		message = valores;
+//		String valores = Arrays.toString(aux);
+//		message = valores;
 		return message;
 	}
 
@@ -21,7 +27,7 @@ public class EjClase1 {
 		int values[] = { 1, 2, 3, 4, 5, };
 		System.out.println(Arrays.toString(values));
 
-		System.out.println(inReverse(values));
+		System.out.println(inReverse(new int[] { 1, 2, 3, 4, 5 }));
 
 	}
 
